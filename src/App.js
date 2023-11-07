509af22b8382250440a2b2bb9d99eb97ff4692e7
toshi-Devs <trahim87mustapha@gmail.com>
2023-11-07 21:44:42 +0200

    Header designed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Toshi-Devs</h1>
+      
+      <Header />
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        
+        <div className='header-text'>
+            <h1>TODO</h1>
+            <img src='./images/icon-sun.svg' alt='logo'></img>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
