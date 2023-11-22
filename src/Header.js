0f0356678e6496ca3074354667f9d157a0026d0d
toshi-Devs <trahim87mustapha@gmail.com>
2023-11-22 16:20:13 +0200

    Now the icons work, switch between different types

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './Header.css'
 
+
 function Header() {
+
+    const [theme, setTheme] = useState('light'); // 'light' | 'dark'
+    
+
   return (
     <div className='header'>
         
         <div className='header-text'>
             <h1>TODO</h1>
-            <img src='./images/icon-sun.svg' alt='logo'></img>
+            <button 
+            className='btn'
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            aria-label={`Switch to ${theme === 'light' ?
+             'dark' : 'light'} mode`}
+            >
+              {theme === 'dark' ? 
+              <img src='./images/icon-sun.svg'/>
+              : <img src='./images/icon-moon.svg'/>}
+            </button>
             
         </div>
     </div>
--- src/ThemeSwitcher.js
+++ src/ThemeSwitcher.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import { React, useState} from 'react'
+import styles from './ThemeSwitcher.module.css'
+import CloseIcon from '@mui/icons-material/Close';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
 
 const ThemeSwitcher = () => {
+
+  const [isColorPicking, setIsColorPicking] = useState(false);
+
   return (
-    <div>ThemeSwitcher</div>
+    <div className={styles.wrapper}>
+      {
+        isColorPicking 
+        ? (
+          <>
+          <button 
+          className={`btn ${styles.close}`}
+          aria-label='Disable color picker'
+          onClick={() => setIsColorPicking(false)}
+          > <CloseIcon />
+          </button>
+          <input 
+          className={styles.picker}
+          type='range'
+
+          />
+          </>
+        )
+        : (
+          <div className={styles.btns}>
+            <button 
+            className='btn'
+            aria-label='Enable color picker'
+            onClick={() => setIsColorPicking(true)}
+            > <ColorLensIcon/>
+            </button>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
